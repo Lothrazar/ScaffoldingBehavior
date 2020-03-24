0ae4fb246b9b3426bda4496c21b066c739a5cfdc
lothrazar <samson.bassett@gmail.com>
2020-03-23 22:25:21 -0700

    rail speedbuild just like ladder

--- src/main/java/com/lothrazar/ladderpower/ConfigManager.java
+++ src/main/java/com/lothrazar/ladderpower/ConfigManager.java
@@ -5,19 +5,35 @@ import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 
 public class ConfigManager {
 
   private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
   private static ForgeConfigSpec COMMON_CONFIG;
-  private static BooleanValue TOOLTIPS;
+  //  public static BooleanValue FASTUP;
+  public static BooleanValue LADDERBUILD;
+  public static BooleanValue AUTOBUILDINVALID;
+  public static IntValue AUTOBUILDRANGE;
+  public static BooleanValue RAILBUILD;
   static {
     initConfig();
   }
 
   private static void initConfig() {
     COMMON_BUILDER.comment("General settings").push(ExampleMod.MODID);
-    TOOLTIPS = COMMON_BUILDER.comment("Testing config Tooltip").define("itemTooltip", true);
+    //    FASTUP = COMMON_BUILDER.comment(
+    //        "While on a ladder and looking up, you will auto climb the ladder.  Still lets you sneak to stop moving.  Disable if you have auto-sprint or overlapping ladder features from other mods.   ")
+    //        .define("ladder.autoClimbingUp", true);
+    RAILBUILD = COMMON_BUILDER.comment("Auto Rail Building: Place a rail on a rail and it will build out depending where player is facing")
+        .define("rails.autoBuild", true);
+    LADDERBUILD = COMMON_BUILDER.comment("Auto Ladder Building: Place a ladder on a ladder and it will build up or down depending where player is facing")
+        .define("ladder.autoBuild", true);
+    AUTOBUILDRANGE = COMMON_BUILDER.comment("Auto Ladder Building: vertical range away from source placement")
+        .defineInRange("ladder.autoBuildRange", 16, 1, 64);
+    AUTOBUILDINVALID = COMMON_BUILDER.comment("Auto Ladder Building: true means allow invalid building places such as floating in midair")
+        .define("ladder.autoBuildInvalid", true);
+    //RAILS SAME THING 
     COMMON_BUILDER.pop();
     COMMON_CONFIG = COMMON_BUILDER.build();
   }
@@ -30,9 +46,5 @@ public class ConfigManager {
         .build();
     configData.load();
     COMMON_CONFIG.setConfig(configData);
-  }
-
-  public boolean tooltips() {
-    return TOOLTIPS.get();
   }
 }
--- src/main/java/com/lothrazar/ladderpower/ExampleMod.java
+++ src/main/java/com/lothrazar/ladderpower/ExampleMod.java
@@ -7,16 +7,17 @@ import com.lothrazar.ladderpower.setup.IProxy;
 import com.lothrazar.ladderpower.setup.ServerProxy;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.GrassBlock;
 import net.minecraft.block.LadderBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
@@ -46,8 +47,7 @@ public class ExampleMod {
   }
 
   private void setup(final FMLCommonSetupEvent event) {
-    //now all blocks/items exist
-    config.tooltips();//config values used like this
+    //now all blocks/items exist 
   }
 
   @SubscribeEvent
@@ -70,10 +70,37 @@ public class ExampleMod {
     World world = event.getWorld();
     BlockState stateOG = world.getBlockState(pos);
     ItemStack held = event.getItemStack();//player.getHeldItem(event.getHand());
+    if (ConfigManager.RAILBUILD.get() && held.getItem() == Items.RAIL && stateOG.getBlock() == Blocks.RAIL) {
+      Direction facing = player.getHorizontalFacing();
+      // ok then 
+      //build 
+      for (int i = 1; i < ConfigManager.AUTOBUILDRANGE.get(); i++) {
+        BlockPos posCurrent = pos.offset(facing, i);
+        //here? 
+        BlockState newLadder = Blocks.RAIL.getDefaultState();
+        //        newLadder = newLadder.with(RailBlock.SHAPE, stateOG.get(RailBlock.SHAPE));
+        GrassBlock x;
+        boolean replaceHere = world.getBlockState(posCurrent).getMaterial().isReplaceable();
+        //        System.out.println(replaceHere + " can be replaced " + world.getBlockState(posCurrent));
+        if (replaceHere
+            && newLadder.isValidPosition(world, posCurrent)) {
+          //water logged if its wet
+          //          boolean isWater = world.getFluidState(posCurrent).getFluid() == Fluids.WATER
+          //              || world.getFluidState(posCurrent).getFluid() == Fluids.FLOWING_WATER;
+          //          newLadder = newLadder.with(BlockStateProperties.WATERLOGGED, Boolean.valueOf(isWater));
+          if (world.setBlockState(posCurrent, newLadder)) {
+            if (!player.isCreative()) {
+              held.shrink(1);
+            }
+            break;
+          }
+        }
+      }
+    }
     //if i used ladder on top of a ladder
-    if (held.getItem() == Items.LADDER && stateOG.getBlock() == Blocks.LADDER) {
+    if (ConfigManager.LADDERBUILD.get() && held.getItem() == Items.LADDER && stateOG.getBlock() == Blocks.LADDER) {
       //then ooo hot stuff 
-      for (int i = 1; i < 10; i++) {
+      for (int i = 1; i < ConfigManager.AUTOBUILDRANGE.get(); i++) {
         // 
         BlockPos posCurrent = (player.rotationPitch < 0) ? pos.up(i) : pos.down(i);
         BlockState stateCurrent = world.getBlockState(posCurrent);
@@ -82,40 +109,40 @@ public class ExampleMod {
         }
         LadderBlock b;
         BlockState newLadder = Blocks.LADDER.getDefaultState();
-        // ok 
         newLadder = newLadder.with(LadderBlock.FACING, stateOG.get(LadderBlock.FACING));
-        if (newLadder.isValidPosition(world, posCurrent)) {
+        // ok 
+        // build it!
+        if (ConfigManager.AUTOBUILDINVALID.get()
+            || newLadder.isValidPosition(world, posCurrent)) {
           //water logged if its wet
-          newLadder = newLadder.with(BlockStateProperties.WATERLOGGED, Boolean.valueOf(world.getFluidState(posCurrent).getFluid() == Fluids.WATER));
-        }
-        if (world.setBlockState(posCurrent, newLadder)) {
-          if (!player.isCreative()) {
-            held.shrink(1);
-          }
-          break;
-        }
-      }
-    }
-  }
-
-  @SubscribeEvent
-  public void onPlayerTick(LivingUpdateEvent event) {
-    boolean fastLadderClimb = true;
-    if (fastLadderClimb) {
-      if (event.getEntityLiving() instanceof PlayerEntity) {
-        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-        if (player.isOnLadder() && !player.isCrouching() && player.moveForward == 0) {
-          //move up faster without 'w'
-          if (player.rotationPitch < LADDER_ROTATIONLIMIT) {
-            //up 
-            player.addVelocity(0, LADDER_SPEED, 0);
-          }
-          else if (player.rotationPitch > -1 * LADDER_ROTATIONLIMIT / 2) {
-            //up 
-            player.addVelocity(0, -1 * LADDER_SPEED, 0);
+          boolean isWater = world.getFluidState(posCurrent).getFluid() == Fluids.WATER
+              || world.getFluidState(posCurrent).getFluid() == Fluids.FLOWING_WATER;
+          newLadder = newLadder.with(BlockStateProperties.WATERLOGGED, Boolean.valueOf(isWater));
+          if (world.setBlockState(posCurrent, newLadder)) {
+            if (!player.isCreative()) {
+              held.shrink(1);
+            }
+            break;
           }
         }
       }
     }
   }
+  //
+  //  @SubscribeEvent
+  //  public void onPlayerTick(LivingUpdateEvent event) {
+  //    if (ConfigManager.FASTUP.get()) {
+  //      if (event.getEntityLiving() instanceof PlayerEntity) {
+  //        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+  //        if (player.isOnLadder() && !player.isCrouching() && player.moveForward == 0) {
+  //          //move up faster without 'w'
+  //          if (ConfigManager.FASTUP.get() &&
+  //              player.rotationPitch < LADDER_ROTATIONLIMIT) {
+  //            //up 
+  //            player.addVelocity(0, LADDER_SPEED, 0);
+  //          }
+  //        }
+  //      }
+  //    }
+  //  }
 }
